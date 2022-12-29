20c533d0d1493649b1f268c69d3de7c6a6827d9b
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2022-12-29 10:27:14 +0800

    fix: remove unnecessary if statement

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import { client } from './connection'
 import config from './config'
 import { messageHandler } from './services'
 
-const { DISCORD_BOT_TOKEN, DISCORD_CHANNEL_ID } = config
+const { DISCORD_BOT_TOKEN } = config
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+client.once('ready', (c) => {
+  console.log(`Logged in as ${c.user.tag}!`)
 })
 
 client.on('messageCreate', async (message) => {
   if (message.author?.bot) return
-  if (message.channel.id === DISCORD_CHANNEL_ID) messageHandler(message)
+  messageHandler(message)
 })
 
 client.login(DISCORD_BOT_TOKEN)
